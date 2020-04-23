08a5ca35228721574f5041fb07137280426f329c
BGM <bgmp00@protonmail.com>
2020-04-22 20:18:06 -0400

    Gracefully resolve error pages

--- config/routes.rb
+++ config/routes.rb
@@ -14,13 +14,17 @@ Rails.application.routes.draw do
   # Staff page
   get 'staff' => 'staff#index'
 
+  # Placeholder view so clicking on staff heads doesn't overload the database
+  get 'profiles' => 'profiles#index'
+
   # Image gallery
   get 'gallery' => 'gallery#index'
 
   # Medieval Secrets
   get 'medievo' => 'el_medievo#index'
 
-  get '/404', to: "errors#not_found"
-  get '/422', to: "errors#unacceptable"
-  get '/500', to: "errors#server_error"
+  match '/404' => 'errors#not_found', via: :all
+  match '/422' => 'errors#illegal', via: :all
+  match '/500' => 'errors#internal_server_error', via: :all
+
 end
