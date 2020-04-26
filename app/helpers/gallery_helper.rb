ac50924da88e2177efca3f0dccf7b3ffacda49d2
BGM <bgmp00@protonmail.com>
2020-04-26 03:12:22 -0400

    Fix a ton of typos

--- app/helpers/gallery_helper.rb
+++ app/helpers/gallery_helper.rb
@@ -1,5 +1,5 @@
 module GalleryHelper
-  REMOTES_IMAGES_URLS = ['https://i.imgur.com/Es8VFS5.jpg',
+  REMOTE_IMAGES_URLS = ['https://i.imgur.com/Es8VFS5.jpg',
                          'https://i.imgur.com/XHszGFm.jpg',
                          'https://i.imgur.com/fjMiq2f.jpg',
                          'https://i.imgur.com/D84F935.jpg',
