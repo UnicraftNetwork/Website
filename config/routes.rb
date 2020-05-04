fa7506c60b89e0b8efac0f5edbb9c8c2556469f6
BGM <bgmp00@protonmail.com>
2020-05-04 19:41:29 -0400

    Add re-direction for /shop route
    Resolves #10

--- config/routes.rb
+++ config/routes.rb
@@ -26,6 +26,9 @@ Rails.application.routes.draw do
   # Contact
   get 'contact' => 'application#contact'
 
+  # Shop re-direct
+  get "/shop" => redirect("https://udecesports.buycraft.net/")
+
   match '/404', to: 'errors#not_found', via: :all
   match '/422', to: 'errors#illegal', via: :all
   match '/500', to: 'errors#internal_server_error', via: :all
