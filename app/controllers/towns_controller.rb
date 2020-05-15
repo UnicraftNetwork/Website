879442d2f20a7bec90a537841358e96924a611b6
BGM <bgmp00@protonmail.com>
2020-05-14 23:50:37 -0400

    Sort towns based on conquered territory

--- app/controllers/towns_controller.rb
+++ app/controllers/towns_controller.rb
@@ -1,5 +1,5 @@
 class TownsController < ApplicationController
   def index
-    @towns = Town.order(:name).page(params[:page])
+    @towns = Town.order(:conquered).sort { |a, b| b[:bar] <=> a[:bar] }
   end
 end
