e385d9b1c8bdfbcd8ba4e9e9b3335818a6d0b007
BGM <bgmp00@protonmail.com>
2020-05-31 03:28:13 -0400

    Fix weird behaviour with stats's table indexes

--- app/controllers/pgm_players_controller.rb
+++ app/controllers/pgm_players_controller.rb
@@ -1,5 +1,6 @@
 class PgmPlayersController < ApplicationController
   def index
     @players = PgmPlayer.order('kills DESC').page(params[:page]).per(20)
+    @count = (@players.current_page - 1) * @players.limit_value + 1
   end
 end
