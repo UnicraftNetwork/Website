679839329de5a6189afba411a4b48f61ca0be269
BGM <bgmp00@protonmail.com>
2020-05-31 23:45:21 -0400

    Add basic sorting for kills & deaths

--- app/controllers/pgm_players_controller.rb
+++ app/controllers/pgm_players_controller.rb
@@ -1,6 +1,11 @@
 class PgmPlayersController < ApplicationController
   def index
-    @players = PgmPlayer.order('kills DESC').page(params[:page]).per(20)
+    params[:sort] ||= 'kills'
+    @players = PgmPlayer.order(params[:sort] + ' DESC').page(params[:page]).per(20)
     @count = (@players.current_page - 1) * @players.limit_value + 1
+    @sorts = {
+      kills: 'Kills',
+      deaths: 'Deaths'
+    }
   end
 end
