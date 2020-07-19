4d71df9ba2874f271b7e09e0e68c6cff92567f9b
José Benavente <jose@bgmp.cl>
2020-07-18 23:44:04 -0400

    Fix profile rendering issues for unlinked players

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,4 +1,30 @@
 module UsersHelper
+
+  # Resolves the requested statistic from the provided
+  # record, or '?' if nil
+  def resolve_stat(pgm_record, stat)
+    return '?' if pgm_record.nil?
+
+    case stat
+    when 'kills'
+      pgm_record&.kills
+    when 'deaths'
+      pgm_record&.deaths
+    when 'kd'
+      pgm_record&.kd
+    when 'kk'
+      pgm_record&.kk
+    when 'wools'
+      pgm_record&.wools
+    when 'monuments'
+      pgm_record&.monuments
+    when 'cores'
+      pgm_record&.cores
+    else
+      '?'
+    end
+  end
+
   module Profile
     class << self
       def render(content)
