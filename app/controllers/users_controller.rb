6958aec066f8c195a907732dbe488eb0e878053b
José Benavente <jose@bgmp.cl>
2020-07-21 14:59:41 -0400

    Begin support for staff coloured names & badges

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,8 @@ class UsersController < ApplicationController
   def show
     @user = User.find_by!(name: params[:name])            # User by profile name.
     @pgm_record = PgmRecord.find_by_uuid(@user.uuid)      # @user's pgm record, or nil if it doesn't have a Minecraft account linked.
+
+    staff_profile = Staff.find_by_uuid(@user.uuid)
+    @groups = staff_profile.nil? ? [] : staff_profile.groups.sort_by(&:priority)
   end
 end
