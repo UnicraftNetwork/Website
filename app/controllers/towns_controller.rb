8eecfc27186ebdddee0bc6947ecbb9f480bbb622
José Benavente <jose@bgmp.cl>
2020-07-21 22:20:28 -0400

    Paginate town entries

--- app/controllers/towns_controller.rb
+++ app/controllers/towns_controller.rb
@@ -1,5 +1,7 @@
 class TownsController < ApplicationController
   def index
-    @towns = Town.all.sort_by { |town| town.residents.split('#').size }.reverse!
+    @towns = Kaminari.paginate_array(
+        Town.all.sort_by { |town| town.residents.split('#').size }.reverse!
+    ).page(params[:page]).per(12)
   end
 end
