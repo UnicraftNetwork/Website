f7f491742d4e0bf454f1ad5415f931b8c77e22df
José Benavente <jose@bgmp.cl>
2020-07-22 02:01:50 -0400

    Fix tiny formatting mistake

--- app/controllers/towns_controller.rb
+++ app/controllers/towns_controller.rb
@@ -1,7 +1,7 @@
 class TownsController < ApplicationController
   def index
     @towns = Kaminari.paginate_array(
-        Town.all.sort_by { |town| town.residents.split('#').size }.reverse!
+      Town.all.sort_by { |town| town.residents.split('#').size }.reverse!
     ).page(params[:page]).per(12)
   end
 end
