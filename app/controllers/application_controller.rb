66d6e731f397dd4956d18f0975d2cf0c54c5579c
José Benavente <jose@bgmp.cl>
2020-10-19 03:51:40 -0300

    Display friends on user profiles

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,11 +5,17 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery prepend: true
 
+  helper_method :uuid_to_nickname
   helper_method :uuid_to_profile_link
   helper_method :user_groups
   helper_method :username_color
   helper_method :username_color_of_uuid
   helper_method :resolve_minecraft_nick
+
+  def uuid_to_nickname(uuid)
+    user = User.find_by(uuid: uuid)
+    user.nil? ? nil : user.name
+  end
 
   def user_groups(user)
     staff_profile = user.uuid.nil? ? nil : Staff.find_by_uuid(user.uuid)
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,6 @@ class UsersController < ApplicationController
   def show
     @user = User.find_by!(name: params[:name])            # User by profile name.
     @pgm_record = PgmRecord.find_by_uuid(@user.uuid)      # @user's pgm record, or nil if it doesn't have a Minecraft account linked.
+    @friends = @user.friends.split("#")
   end
 end
