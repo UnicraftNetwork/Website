57e5a359e0a9a0bb5012fdd9b3934e30fff18895
José Benavente <jose@bgmp.cl>
2020-10-21 21:04:08 -0300

    Explicitly declare a default value for user friends

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,13 @@ class User < ApplicationRecord
 
   validates :name, uniqueness: true, length: { minimum: 3, maximum: 16 }
 
+  after_initialize :set_defaults
+
   def to_param
     name
+  end
+
+  def set_defaults
+    self.friends ||= ''
   end
 end
